1a74601708e3500ecba7724793922dda8ef0df0b
Roopal <54892036+Rmittal21@users.noreply.github.com>
2019-10-04 21:57:01 +0530

    Update atcoder_2.cpp

--- atcoder_2.cpp
+++ atcoder_2.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
 int main(){
 	int a,b;
+	//Input two integer variables
 	cin>>a;
 	cin>>b;
-	int i=1;
+	int i=1;//Initializing i with 1
 	while(a==b){
 		cout<<i;
 	}
@@ -14,6 +15,6 @@ int main(){
 		continue;
 	}
 		cout<<i;
-
+	//returning 0 value to OS
 	return 0;
 }
